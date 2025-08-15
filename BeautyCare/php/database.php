d6c5f76870f2c882fc9447fb066450c966df5960
ThienNam13 <thiennamng308@gmail.com>
2025-08-15 19:13:23 +0700

    Trạng thái đơn hàng theo từng Case

--- BeautyCare/php/database.php
+++ BeautyCare/php/database.php
@@ -6,6 +6,7 @@ $dbname = "myphamdb";
 
 // Kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
+$conn->query("SET time_zone = '+07:00'");
 
 // Kiểm tra kết nối
 if ($conn->connect_error) {
